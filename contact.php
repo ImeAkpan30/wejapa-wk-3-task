e9d79ffdcad0783ce2648811345f126697650104
ImeAkpan30 <imeakpan5050@gmail.com>
2020-08-09 18:29:54 +0100

    added sessions

--- contact.php
+++ contact.php
@@ -1,3 +1,17 @@
+<?php 
+    session_start();
+
+    $first_name = $_SESSION['first_name'];
+    $second_name = $_SESSION['second_name'];
+    $email = $_SESSION['email'];
+    $dob = $_SESSION['date_of_birth'];
+    $color = $_SESSION['color'];
+    $gender = $_SESSION['gender'];
+    $department = $_SESSION['department'];
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,60 +22,42 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 
 </head>
-<body style="background-color:<?php echo $_POST["color"]; ?>">
+<body style="background-color:<?php echo $color; ?>">
 
-<div class="card text-white text-center mt-5 shadow-lg" style="max-width: 18rem; margin-left:40%; background-color:<?php echo $_POST["color"]; ?>">
+<div class="card text-white text-center mt-5 shadow-lg" style="max-width: 18rem; margin-left:40%; background-color:<?php echo $color; ?>">
   <div class="card-header"><h5>User Details</h5></div>
   <div class="card-body">
       
     
     <p class="card-text">
-    <?php
-        $first_name = $_POST['first_name'];
-        $second_name = $_POST['second_name'];
-        $email = $_POST['email'];
-        $dob = $_POST['date_of_birth'];
-        $color = $_POST['color'];
-        $gender = $_POST['gender'];
-        $department = $_POST['department'];
-        $password = $_POST['password'];
-
-
-        
-            echo "<b> First Name: </b>  ". $first_name;  
-            
-            echo "<br><br>";
-            
+        <?php  
+            echo "<b> First Name: </b>  ". $first_name; 
+        ?> <br><br>
+   
+        <?php 
             echo "<b> Second Name: </b>  ". $second_name;
+        ?><br><br>
 
-            echo "<br><br>";
-
+        <?php
             echo "<b> Email: </b>  ". $email;
+        ?><br><br>
 
-            echo "<br><br>";
-
+        <?php 
             echo "<b> Date Of Birth: </b>  ". $dob;
+        ?><br><br>
 
-            echo "<br><br>";
-
+        <?php
             echo "<b> Color: </b>  ". $color;
+        ?><br><br>
 
-            echo "<br><br>";
-
+        <?php
             echo "<b> Gender: </b>  ". $gender;
+        ?><br><br>
 
-            echo "<br><br>";
-
+        <?php
             echo "<b> Department: </b>  ". $department;
-
-            echo "<br><br>";
-
-            echo "<b> Password: </b>  ". $password;
-
-               
         ?>
-
-
+        
     </p>
   </div>
 </div>
